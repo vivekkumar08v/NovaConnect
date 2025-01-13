bc396626a2611f74210ba045eb6c841efa6b5b6a
Vivek Kumar <vivek08012003@gmail.com>
2025-01-13 19:25:46 +0530

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/header'
+import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopConsultants from '../components/TopConsultants'
 import Banner from '../components/Banner'
